aa0d2155596600ec06bc6f16ef37773dba40fe0a
Brightini <brightiniabasi@gmail.com>
2023-08-21 23:24:37 +0100

    Updated code :zap:

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 """ This program counts the number of words in a text """
 
-with open("books/frankenstein.txt", "r") as file:
-	file_content = file.read()
+def main():
+	book_path = "books/frankenstein.txt"
+	text = get_text(book_path)
+	count = get_word_count(text)
+	print(count)
 
-word_count = len(file_content.split())
-print(word_count)
+
+def get_word_count(text):
+	word_count = text.split()
+	return len(word_count)
+
+def get_text(path):
+	with open(path, "r") as file:
+		return file.read()
+
+main()
